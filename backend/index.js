78eb6c4a505e2e5634b656a99ff7296079dd8895
rekhayadav <rekha.ee026@gmail.com>
2023-04-18 18:19:55 +0530

    added

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+require("dotenv").config()
+const { connection } = require("./configs/db")
+const { userRoutes } = require("./routes/users.routes")
+
+
+const app = express()
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.send("homepage")
+
+})
+app.use("/users",userRoutes)
+
+app.listen(process.env.port, async () => {
+    try {
+        await connection
+        console.log("connected to DB")
+    }
+    catch (err) {
+        console.log("error while connected to db")
+        console.log(err)
+    }
+})
